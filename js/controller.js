e8f1f84810a5620db7d4b61f4c133215b23460fb
chapperz <Ben@benchaplinsmbp.home>
2014-01-28 21:12:02 +0000

    First experiment with github api.

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,14 @@
+function FeedCtrl($scope, $http) {
+    var url = 'https://api.github.com/orgs/chapperz/repos';
+    var url2 = 'https://api.github.com/users/:chapperz';
+
+    $http({method: 'GET', url: url}).
+        success(function(data, status, headers, config) {
+            $scope.data = data;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
 }
