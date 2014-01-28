8800d692da90f4e045fbfa66b9059c663ba827af
chapperz <Ben@benchaplinsmbp.home>
2014-01-28 22:43:22 +0000

    all the feeds needed

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,40 @@
-function FeedCtrl($scope, $http) {
-    var url = 'https://api.github.com/orgs/chapperz/repos';
-    var url2 = 'https://api.github.com/users/:chapperz';
 
+
+function FeedCtrl($scope, $http) {
+    var url,
+        gitUrl = 'https://api.github.com/';
+        user = 'p-m-p',
+        repo = 'jquery-box-slider';
+
+    $scope.repos = new Array();
+
+    url = gitUrl + 'users/' + user + '/repos';
     $http({method: 'GET', url: url}).
         success(function(data, status, headers, config) {
-            $scope.data = data;
+            $scope.repos = data;
         }).
         error(function(data, status, headers, config) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
 
+    url = gitUrl + 'repos/' + user + '/' + repo + '/languages';
+    $http({method: 'GET', url: url}).
+        success(function(data, status, headers, config) {
+            $scope.jqueryRepo = data;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+    url = gitUrl + 'repos/' + user + '/' + repo + '/readme';
+    $http({method: 'GET', url: url}).
+        success(function(data, status, headers, config) {
+            $scope.readme = data;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
 }
